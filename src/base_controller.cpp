f8bad0b2a56ee9993a0c93c82035f7e5d28ad9c2
curellich <66680671+curellich@users.noreply.github.com>
2021-09-27 17:49:36 -0300

    Update base_controller.cpp
    Se el movimiento discreto a la función MotorDriver.

--- src/base_controller.cpp
+++ src/base_controller.cpp
@@ -1,4 +1,5 @@
 #include "../include/base_controller.hpp"
+#include "../include/MovimientoDiscreto.hpp"
 
 BaseController::BaseController() {
     // Manejador del Nodo
@@ -7,7 +8,8 @@ BaseController::BaseController() {
     private_n = ros::NodeHandle("~");
     // Inicializacion de las subscripciones
     twist = n.subscribe<geometry_msgs::Twist>("cmd_vel", 10, &BaseController::twistMsgCallback, this);
-
+    //------------>
+    movimiento = MovimientoDiscreto();
 
     // Inicializacion de las publicaciones
     tracksNode =  n.advertise<motors_panda::Motors>("tracks_duty_cycle", 10);
@@ -26,6 +28,7 @@ void BaseController::twistMsgCallback(const geometry_msgs::Twist::ConstPtr& msg)
 
 void BaseController::motorDriver(const double x, const double z){
     // aca mandamos los mensajes a el driver de las ruedas
+    /*
     double fast_track;
     double slow_track;
     fast_track = std::abs(x);
@@ -44,6 +47,13 @@ void BaseController::motorDriver(const double x, const double z){
         motorMsg.data_r = slow_track;
         tracksNode.publish(motorMsg);
     }
+    */
+    //------------->
+    movimiento.realizarMovimientoDiscreto((float)x,(float)z);
+
+    motorMsg.data_l = movimiento.getRueda().izquierda;
+    motorMsg.data_r = movimiento.getRueda().derecha;
+    tracksNode.publish(motorMsg);
 }
 
 int main(int argc, char **argv) {
