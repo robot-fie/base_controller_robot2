575465b19cc2a567ea139cc0a83c1d97b718d02a
Carlos Maceira <carlosmgc2003@gmail.com>
2021-11-10 16:04:15 -0300

    Eliminados resabios deprecados de codigo

--- src/base_controller.cpp
+++ src/base_controller.cpp
@@ -10,7 +10,6 @@ BaseController::BaseController() {
     twist = n.subscribe<geometry_msgs::Twist>("cmd_vel", 10, &BaseController::twistMsgCallback, this);
     //------------>
     movimiento = MovimientoDiscreto();
-
     // Inicializacion de las publicaciones
     tracksNode =  n.advertise<motors_panda::Motors>("tracks_duty_cycle", 10);
 }
@@ -20,39 +19,13 @@ void BaseController::twistMsgCallback(const geometry_msgs::Twist::ConstPtr& msg)
     const double x = msg->linear.x;
     // Tomo la varicion en azimut del vector velocidad 
     const double z = msg->angular.z;
-    // Teniendo los datos del Joystick llamamos a motor driver a ver que hacemos.
-    // De esta manera solo se impacta en los motores si realmente hay ordenes
     motorDriver(x,z);
 }
 
 
 void BaseController::motorDriver(const double x, const double z){
-    // aca mandamos los mensajes a el driver de las ruedas
-    /*
-    double fast_track;
-    double slow_track;
-    fast_track = std::abs(x);
-    slow_track = std::abs(x * (1 - std::abs(z)));
-    */
-   motors_panda::Motors motorMsg;
-   /*
-    if(z == 0.0) {
-        motorMsg.data_l = fast_track;
-        motorMsg.data_r = fast_track;
-        tracksNode.publish(motorMsg);
-    } else if (z < 0.0) {
-        motorMsg.data_l = slow_track;
-        motorMsg.data_r = fast_track;
-        tracksNode.publish(motorMsg);
-    } else if (z > 0.0) {
-        motorMsg.data_l = fast_track;
-        motorMsg.data_r = slow_track;
-        tracksNode.publish(motorMsg);
-    }
-    */
-    //------------->
+    motors_panda::Motors motorMsg;
     movimiento.realizarMovimientoDiscreto((float)x,(float)z);
-
     motorMsg.data_l = movimiento.getRueda().izquierda;
     motorMsg.data_r = movimiento.getRueda().derecha;
     tracksNode.publish(motorMsg);
